75f4806a0db034aef25683d1d06aefd194af34a3
dimaPolonez <dima-barxat@yandex.ru>
2022-06-28 11:51:52 +0300

    feat: finish app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+
+let strN = null;
+
+if (n < 0) {
+ n *= -1; 
+ strN = String(n);  
+ return strN.split('').reverse().join('');
+} else {
+    strN = String(n);  
+    return strN.split('').reverse().join('');
 }
+
+}
+
+
